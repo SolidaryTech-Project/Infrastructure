6e97a2378de32110422cadfc1bb48f217aa9441b
Lucas <morganis1411@gmail.com>
2026-06-21 01:18:08 -0300

    fix: updating secret

--- Environments/Development/modules.tf
+++ Environments/Development/modules.tf
@@ -140,7 +140,7 @@ module "secrets_manager" {
   secrets = {
     shared-db-url = {
       description = "Shared database connection URL"
-      value       = module.rds_postgres.db_instance_endpoint
+      value       = module.databases.db_instance_endpoint
       service_tag = "shared-db"
     }
     donation-sqs-url = {
